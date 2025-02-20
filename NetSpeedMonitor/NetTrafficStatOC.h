d04044b575e638320b02511a70482c58a547428b
elegracer <elegracer@gmail.com>
2025-02-21 01:48:06 +0800

    fix: crash for non primitive type property in oc

--- NetSpeedMonitor/NetTrafficStatOC.h
+++ NetSpeedMonitor/NetTrafficStatOC.h
@@ -4,11 +4,11 @@
 
 
 @interface NetTrafficStatOC : NSObject
-@property (nonatomic, assign) NSNumber *delta_ts_sec;
+@property (nonatomic, strong) NSNumber *delta_ts_sec;
 @property (nonatomic, assign) NSInteger delta_ibytes;
 @property (nonatomic, assign) NSInteger delta_obytes;
-@property (nonatomic, assign) NSNumber *ibytes_per_sec;
-@property (nonatomic, assign) NSNumber *obytes_per_sec;
+@property (nonatomic, strong) NSNumber *ibytes_per_sec;
+@property (nonatomic, strong) NSNumber *obytes_per_sec;
 @end
 
 
